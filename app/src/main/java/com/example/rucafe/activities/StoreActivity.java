f8b65741f4ac1ab6cff873532167f54697632e76
yulying <yy541@scarletmail.rutgers.edu>
2022-04-26 22:46:00 -0400

    Store activity had duplicate onBackPressed() method. Changed Whipped Cream string to W. Cream

--- app/src/main/java/com/example/rucafe/activities/StoreActivity.java
+++ app/src/main/java/com/example/rucafe/activities/StoreActivity.java
@@ -98,17 +98,6 @@ public class StoreActivity extends AppCompatActivity implements AdapterView.OnIt
         }
     }
 
-    @Override
-    public void onBackPressed() {
-        if (checkedOrder == CHECKED) {
-            this.updateUI();
-            checkedOrder = NOT_CHECKED;
-        }
-        else {
-            super.onBackPressed();
-        }
-    }
-
     /**
      * Removes an order from the store orders list.
      */
